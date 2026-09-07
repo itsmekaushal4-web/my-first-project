eb148759ee8733754dbce811a0c18472637d5650
RajaramBhurtel <sabinbhurtel@gmail.com>
2026-09-07 09:29:02 +0545

    email already exists

--- sms/login.php
+++ sms/login.php
@@ -2,6 +2,9 @@
 include('./templates/header.php');
 include('./templates/navbar.php');
 
+if(isset($_GET['success']) && $_GET['success'] == 1) {
+    echo "<div class='alert alert-success'>Registration successful! Please log in.</div>";
+}
 echo "Login Page";
 
 include('./templates/foot.php');
